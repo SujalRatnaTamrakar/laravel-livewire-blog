c87bb08b1bfa20c9cc46c765ef153b667c9d8fce
SujalRatnaTamrakar <sujalratnatamrakar@gmail.com>
2023-06-01 20:19:59 +0545

    add missing tag_id column

--- database/migrations/2023_05_31_162516_create_posts_table.php
+++ database/migrations/2023_05_31_162516_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('category_id');
+            $table->foreignId('tag_id');
             $table->string('slug')->unique();
             $table->string('title');
             $table->string('thumbnail')->nullable();
