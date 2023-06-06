d590147714eb74885ff6b0c26c7b135e9ac2f609
SujalRatnaTamrakar <sujalratnatamrakar@gmail.com>
2023-06-06 21:14:59 +0545

    add package and configs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import swal from 'sweetalert2';
+import 'flowbite';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+window.Swal = swal;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
@@ -19,5 +21,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms,require('flowbite/plugin')],
 };
